64f0d667a3c00854845a0fd1d622532ea5dc833c
brian <briandr@naver.com>
2022-11-20 05:16:30 +0900

    홈 액티비티 임시 서버통신 서비스 구현

--- app/src/main/java/org/sopt/sopkathon_31th/data/remote/api/home/HomeService.kt
+++ app/src/main/java/org/sopt/sopkathon_31th/data/remote/api/home/HomeService.kt
@@ -1,4 +1,10 @@
 package org.sopt.sopkathon_31th.data.remote.api.home
 
+import org.sopt.sopkathon_31th.data.remote.entity.home.ResponseHomeDto
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface HomeService {
+    @GET("/page")
+    fun getHomeMembers(): Call<ResponseHomeDto>
 }
