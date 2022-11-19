1d5c9136b3ead9b59b806f05f67410102a2d8b8a
Cat-JiYoung <kimyamae82@gmail.com>
2022-11-20 07:01:43 +0900

    [add] 코드 추가

---
+++ app/src/main/java/org/sopt/sopkathon_31th/ui/profile/ProfileAdapter.kt
@@ -0,0 +1,23 @@
+package org.sopt.sopkathon_31th.ui.profile
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ProfileAdapter(context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val inflater by lazy { LayoutInflater.from(context) }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
